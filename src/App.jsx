3aac53c3a3f2a1ca26970828d49a5d21100307ad
Emmanuel Joel Odhiambo <joel.emmanuel1968@gmail.com>
2025-03-04 18:46:25 +0300

    made the website responsive

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Routes, Route, useNavigate } from 'react-router-dom'
 import Login from './pages/Login/Login'
 import { onAuthStateChanged } from 'firebase/auth'
 import { auth } from './firebase'
+import { ToastContainer, toast } from 'react-toastify';
 
 const App = () => {
 
@@ -24,6 +25,7 @@ const App = () => {
 
   return (
     <div>
+      <ToastContainer theme='dark'/>
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/login' element={<Login/>}/>
