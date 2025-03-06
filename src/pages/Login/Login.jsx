34d280c5ec5af1ff5e6436de8a8c59b013669abe
Emmanuel Joel Odhiambo <joel.emmanuel1968@gmail.com>
2025-03-06 18:10:42 +0300

    fixed login page

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -34,9 +34,9 @@ const Login = () => {
         <form>
           {signstate === "Sign Up" ? (
             <input value={name} onChange={(e)=>{setName(e.target.value)}} type="text" placeholder="Your name" />
-          ) : (
+          ) : 
             <></>
-          )}
+          }
           <input value={email} onChange={(e)=>{setEmail(e.target.value)}} type="email" placeholder="Email" />
           <input value={password} onChange={(e)=>{setPassword(e.target.value)}}  type="password" placeholder="Password" />
           <button onClick={user_auth} type="submit">{signstate}</button>
